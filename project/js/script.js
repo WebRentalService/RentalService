48a26dcdf3514c83890093af8ae4ff48663f72cb
KimSeungHwan <shk2000v@gmail.com>
2020-10-07 00:00:32 +0900

    bootstrap update

--- project/js/script.js
+++ project/js/script.js
@@ -3,3 +3,8 @@ var hw = document.getElementById('hw');
 hw.addEventListener('click', function(){
     alert('Hello world');
 })
+
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })

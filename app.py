2e54edb50f4beddcdf75ecc574924e1e4c05f39b
mtp0303 <tahz0303@gmail.com>
2020-10-15 17:59:51 +0900

    login

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
-import hashlib
-import bcrypt
+from flask import Flask, render_template, request, redirect, url_for, session, g
 import mariadb
 import sys
 
 app = Flask(__name__)
+#session 사용을 위한 secret_key 정의
+app.secret_key = b'_5#y2L"F4Q8z\\n\\xec]/'
 
 try:
     db = mariadb.connect(
@@ -17,7 +17,6 @@ try:
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
-
 
 cur = db.cursor()
 
@@ -32,7 +31,7 @@ def register():
         
         name = register_info['name']
         username = register_info['username']
-        hashed_password = bcrypt.hashpw(register_info['password'].encode('utf-8'), bcrypt.gensalt())
+        hashed_password = register_info['password']
         phone = register_info['phone']
 
         print(name, username, hashed_password, phone)
@@ -41,42 +40,74 @@ def register():
         db.commit()
         db.close()
 
-
         print(request.url)
-        return redirect(request.url)
 
     return render_template('login.html')
 
+
+@app.route('/login', methods=['POST'])
+def login_info():
+    if request.method == 'POST':
+        login_info = request.form
+        #로그인 사이트에서 username, password 값이 post로 요청되면 아래와 같이 변수에 저장
+        username = login_info['username']
+        password = login_info['password']
+        #쿼리문 실행해서 값을 가져오고 그값을 rows변수에 담는다
+        sql = "SELECT username, hashed_password FROM UserInfo WHERE username=?"
+        cur.execute(sql, (username,))
+        rows = cur.fetchall() #cur.fetchall() -> 쿼리문으로 실행된 데이터베이스 정보를 list로 저장
+        print(len(rows))
+
+        #post로 요청한 username에 값이 데이터 베이스에 있을경우 len(rows)=1, 없을경우 len(lows)=0
+        if len(rows) > 0:
+            print("user info: ", rows[0])
+            #요청한 값이 있을경우 비밀번호 확인
+            if password == rows[0][1]:
+                password_check = True
+                print("password check: ", password_check)
+
+                if password_check == True:
+                    session.clear()
+                    session['loginned_user'] = username
+                    print(session)
+            else:
+                print("비밀번호가 틀립니다.")
+        else:
+            print("회원정보가 없습니다")
+
+    return render_template('choice.html')
+
+#로그인 상태 유무 확인 및 로그인 유지
+#app.before_request -> 사이트가 요청될때마다 route가 실행되기전 항상 먼저 실행된다
+@app.before_request
+def load_logged_in_user():
+    username = session.get('loginned_user') #session 에 'loginned_user' 내용을 가져옴
+    # session 에 값이 없을 경우 g.uer(회원정보) 값은 None, 값이 있을경우 회원정보에 username값을 저장
+    if username is None:
+        g.user = None
+    else:
+        sql = "SELECT username FROM UserInfo WHERE username=?"
+        cur.execute(sql, (username,))
+        user = cur.fetchall()
+        print(user)
+        g.user = user[0][0]
+        print(g.user)
+
 @app.route('/login', methods=['GET'])
-def login():
+def login_page():
     return render_template('login.html')
 
 @app.route('/create', methods=['GET'])
 def create():
     return render_template('create.html')
 
-# @app.route('/login', methods=['POST'])
-# def login():
-#     if request.method == 'POST':
-#         login_info = request.form
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-#         username = login_info['username']
-#         password = login_info['password']
-
-#         sql = "SELECT * FROM UserInfo WHERE username='{}'".format(username)
-#         rows_count = cur.execute(sql)
-
-#         if rows_count > 0:
-#             user_info = cur.fetchone()
-#             print("user_info", user_info)
-
-#             is_pw_correct = bcrypt.checkpw(password.encode('UTF-8'), user_info[2].encode('UTF-8'))
-#             print("password_check: ", is_pw_correct)
-
-#         else:
-#             print("User does not exist")
-
-#         return redirect(request.url)
-
-#     return render_tamplate('login.html')
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
+    # app.secret_key = 'super secret key'
+    # app.config['SEESION_TYPE'] = 'filesystem'
 

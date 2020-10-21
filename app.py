e3f765a4a9817f8599c24ee3d011ce918fe0a158
rhdmstj9119 <68531826+rhdmstj9119@users.noreply.github.com>
2020-10-21 17:45:31 +0900

    go9

--- app.py
+++ app.py
@@ -38,9 +38,11 @@ def register():
         sql = "INSERT INTO UserInfo (name, username, hashed_password, phone) VALUES (?, ?, ?, ?)"
         cur.execute(sql, (name, username, hashed_password, phone)) 
         db.commit()
-        # db.close()
+        db.close()
 
-    return redirect(url_for('login_page'))
+        print(request.url)
+
+    return render_template('login.html')
 
 
 @app.route('/login', methods=['POST'])
@@ -71,20 +73,18 @@ def login_info():
                     return redirect(url_for('calendar'))
             else:
                 flash("비밀번호가 틀립니다.")
-                print("비밀번호가 틀립니다.")
                 return render_template("login.html")
                 
         else:
             flash("회원정보가 없습니다.")
-            print("회원정보가 없습니다.")
             return render_template("login.html")
-    # return redirect(url_for('calendar'))
+    
+    return redirect(url_for('calendar'))
 
 @app.route('/logout')
 def logout():
     session.clear()
-    print(session)
-    return render_template('main.html')
+    return redirect(url_for("login_page"))
 
 #로그인 상태 유무 확인 및 로그인 유지
 #app.before_request -> 사이트가 요청될때마다 route가 실행되기전 항상 먼저 실행된다
@@ -95,34 +95,24 @@ def load_logged_in_user():
     if username is None:
         g.user = None
     else:
-        sql = "SELECT username FROM UserInfo WHERE username=?"
-        cur.execute(sql, (username,))
-        user = cur.fetchall()
-        print(user)
-        g.user = user[0][0]
-        print(g.user)
+        g.user = username
 
-@app.route('/login', methods=['GET'])
+@app.route('/login')
 def login_page():
     return render_template('login.html')
 
-@app.route('/create', methods=['GET'])
+@app.route('/create')
 def create():
     return render_template('create.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 @app.route('/calendar')
 def calendar():
+    if g.user is None:
+        flash("로그인을 먼저 해주세요.")
+        return redirect(url_for("login_page"))
+
     return render_template('calendar.html')
 
 if __name__ == "__main__":
     app.debug=True
-    app.run()
-    # app.secret_key = 'super secret key'
-    # app.config['SEESION_TYPE'] = 'filesystem'
-
-
-
+    app.run(host="0.0.0.0")
